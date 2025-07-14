dcbf2b371bd99e22fd7207337d826a9b735f25c8
tesa <tesamariya2003@gmail.com>
2025-07-15 00:09:09 +0530

    Project Created

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-primary">
+            <nav class="navbar navbar-expand-lg bg-success">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">Todos-App</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
